8392499a67cde42ecef3f2734a716e7368083561
FreedomCoder <matiasbrutti@gmail.com>
2012-11-20 21:35:49 -0800

    More changes to host

--- web/js/helper.js
+++ web/js/helper.js
@@ -23,3 +23,24 @@ function display_search() {
 		searchdiv[0].style.display = "none";
 	}
 }
+
+function fetch_os_image() {
+	var os = document.getElementsByClassName("port_list");
+	var os_name = os[os.length-1].getElementsByTagName("tr")[1].getElementsByTagName("td")[0].textContent;
+	if (os_name.indexOf("Linux") != -1) {
+		document.getElementsByClassName("ip_icon")[0].attributes[1].value = "Linux";
+		return 
+	}
+	if (os_name.indexOf("Window") != -1) {
+		document.getElementsByClassName("ip_icon")[0].attributes[1].value = "Windows";
+		return 
+	}
+	if (os_name.indexOf("Cisco") != -1) {
+		document.getElementsByClassName("ip_icon")[0].attributes[1].value = "Cisco";
+		return 
+	}
+	if (os_name.indexOf("Linksys") != -1) {
+		document.getElementsByClassName("ip_icon")[0].attributes[1].value = "Linksys";
+		return 
+	}
+}
